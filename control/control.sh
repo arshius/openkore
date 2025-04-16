01685381b3402e9c77693109132a16a64bae28fe
Danuys <danuys@nusaciptaalumindo.com>
2025-04-16 15:14:09 +0700

    add script for creating subconfig

---
+++ control/control.sh
@@ -0,0 +1,19 @@
+for x in $@
+do
+    ln arrowcraft.txt $x/arrowcraft.txt
+    ln avoid.txt $x/avoid.txt
+    ln buyer_shop.txt $x/buyer_shop.txt
+    ln chat_resp.txt $x/chat_resp.txt
+    ln consolecolors.txt $x/consolecolors.txt
+    ln eventMacros.txt $x/eventMacros.txt
+    ln items_control.txt $x/items_control.txt
+    ln overallAuth.txt $x/overallAuth.txt
+    ln pickupitems.txt $x/pickupitems.txt
+    ln poseidon.txt $x/poseidon.txt
+	ln priority.txt $x/priority.txt
+	ln responses.txt $x/responses.txt
+	ln routeweights.txt $x/routeweights.txt
+	ln shop.txt $x/shop.txt
+	ln sys.txt $x/sys.txt
+	ln timeouts.txt $x/timeouts.txt
+done
