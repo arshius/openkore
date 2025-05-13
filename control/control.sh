0176a455591a7218f3bb5caaad3f3e85ea8e4ab5
arshius <arsh.bringer@yahoo.com>
2025-05-13 21:43:42 +0700

    fix bug

--- control/control.sh
+++ control/control.sh
@@ -11,20 +11,20 @@ do
 		cp config${x%??}.txt $x/config_.txt
 	fi
     cp mon_control.txt $x/mon_control.txt
-    ln arrowcraft.txt $x/arrowcraft.txt
-    ln avoid.txt $x/avoid.txt
-    ln buyer_shop.txt $x/buyer_shop.txt
-    ln chat_resp.txt $x/chat_resp.txt
-    ln consolecolors.txt $x/consolecolors.txt
-    ln eventMacros.txt $x/eventMacros.txt
-    ln items_control.txt $x/items_control.txt
-    ln overallAuth.txt $x/overallAuth.txt
-    ln pickupitems.txt $x/pickupitems.txt
-    ln poseidon.txt $x/poseidon.txt
-    ln priority.txt $x/priority.txt
-    ln responses.txt $x/responses.txt
-    ln routeweights.txt $x/routeweights.txt
-    ln shop.txt $x/shop.txt
-    ln sys.txt $x/sys.txt
-    ln timeouts.txt $x/timeouts.txt
+    ln -s arrowcraft.txt $x/arrowcraft.txt
+    ln -s avoid.txt $x/avoid.txt
+    ln -s buyer_shop.txt $x/buyer_shop.txt
+    ln -s chat_resp.txt $x/chat_resp.txt
+    ln -s consolecolors.txt $x/consolecolors.txt
+    ln -s eventMacros.txt $x/eventMacros.txt
+    ln -s items_control.txt $x/items_control.txt
+    ln -s overallAuth.txt $x/overallAuth.txt
+    ln -s pickupitems.txt $x/pickupitems.txt
+    ln -s poseidon.txt $x/poseidon.txt
+    ln -s priority.txt $x/priority.txt
+    ln -s responses.txt $x/responses.txt
+    ln -s routeweights.txt $x/routeweights.txt
+    ln -s shop.txt $x/shop.txt
+    ln -s sys.txt $x/sys.txt
+    ln -s timeouts.txt $x/timeouts.txt
 done
